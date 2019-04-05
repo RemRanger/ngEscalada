6b93e2b68f8cb9f3c2da121dcde5368f2860acb3
RemRanger <rxb@ziggo.nl>
2019-04-05 17:28:52 +0200

    Styles.

--- Escalada/wwwroot/styles.js
+++ Escalada/wwwroot/styles.js
@@ -557,7 +557,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\Develop\Escalada\Escalada\src\styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! D:\Develop\ngEscalada\Escalada\src\styles.css */"./src/styles.css");
 
 
 /***/ })
